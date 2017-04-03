dca49d024b527a2a486842aae2f1edc11775f04e
chof09 <lbf @o dfs .com>
2017-04-03 16:25:12 +0200

    Added an option to cancel editing.

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ var edit_form = "<form action='update.php' class='edit-item' method='post'>"
 					+"<input type='text' name='edit-item-field' id='edit-field'>"
 					+"<input type='hidden' name='id-to-edit' id='edit-hidden'>"
 					+"<input type='submit' value='Save' class='submit-change-btn'>"
+					+"<a href='javascript:void(0);' class='cancel-edit-btn' onclick='cancel_edit()'>Cancel</a>"
 				+"</form>";
 
+var oldValue, thisCounter, fullForm;
+
 function edit(counter, id) {
-	var oldValue = document.getElementById(counter).innerHTML;
+	thisCounter = counter;
+	oldValue = document.getElementById(counter).innerHTML;
+
 	document.getElementById(counter).innerHTML = edit_form;
 	document.getElementById("edit-field").value = oldValue;
 	document.getElementById("edit-hidden").value = id;
+	fullForm = document.getElementById(counter).innerHTML;
 	document.getElementById("edit-field").focus();
-	document.getElementById("edit-btn" + counter).innerHTML = "";
+	document.getElementById("edit-btn" + counter).style.display = "none";
 };
 
-function show_edit_btn(number) {
-	var editId = "edit-btn" + number;
-	document.getElementById(editId).style.display = "inline";
+function cancel_edit() {
+	document.getElementById(thisCounter).innerHTML = oldValue;
+	document.getElementById("edit-btn" + thisCounter).style.display = "inline";
+}
+
+function show_edit_btn(counter) {
+	if (document.getElementById(counter).innerHTML != fullForm) {
+		var editId = "edit-btn" + counter;
+		document.getElementById(editId).style.display = "inline";
+	};
 };
 
-function hide_edit_btn(number) {
-	var editId = "edit-btn" + number;
-	document.getElementById(editId).style.display = "none";
+function hide_edit_btn(counter) {
+	if (document.getElementById(counter).innerHTML != fullForm) {
+		var editId = "edit-btn" + counter;
+		document.getElementById(editId).style.display = "none";
+	};
 };
